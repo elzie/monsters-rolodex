848cd29d5cf200ab7d857bc56f403d6839323805
Djan K. Blom <djan@live.dk>
2022-08-06 16:28:39 +0200

    Class Components Done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import CardList from './components/card-list/card-list.component';
 import './App.css';
+import SearchBox from './components/search-box/search-box.component';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    console.log('constructor');
+    // All classes runs the constructor-method before anything else
+    super();
+    // Super calls the constructor-method from any other classes we are extending from
+    this.state = {
+      // key-value pairs
+      monsters: [],
+      searchField: '',
+    };
+  }
+
+  // Lifecycle-methods
+  componentDidMount() {
+    console.log('componentDidMount');
+    // API Call
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response) => response.json())
+      .then((users) =>
+        this.setState(
+          () => {
+            return { monsters: users };
+          },
+          () => {
+            console.log(this.state);
+          }
+        )
+      );
+  }
+
+  onSearchChange = (e) => {
+    const searchField = e.target.value.toLocaleLowerCase();
+
+    this.setState(() => {
+      return { searchField };
+    });
+  };
+
+  render() {
+    console.log('render');
+
+    // Destructuring and getting rid of 'this.'
+    const { monsters, searchField } = this.state;
+    const { onSearchChange } = this;
+
+    const filteredMonsters = monsters.filter((monster) => {
+      //if the name includes the searched value, return true
+      // .includes is not case-sensitive, which is why we lowerCase all text typed in
+      return monster.name.toLocaleLowerCase().includes(searchField);
+    });
+
+    return (
+      <div className="App">
+        <h1 className="app-title">Elzies Monsters Rolodex</h1>
+
+        <SearchBox
+          onChangeHandler={onSearchChange}
+          placeholder="Search Monsters"
+          className="search-box"
+        />
+        <CardList monsters={filteredMonsters}></CardList>
+      </div>
+    );
+  }
 }
 
 export default App;
